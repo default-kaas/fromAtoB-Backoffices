94c981c63e3439e7b1c2eb7399ad8314769b609e
default-kaas <dogen1996@live.nl>
2017-05-11 14:23:14 +0200

    Beging connectie klasse

---
+++ FromAtoBBackoffice/src/fromatobbackoffice/DatabaseConnection.java
@@ -0,0 +1,11 @@
+package fromatobbackoffice;
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static final String USERNAME = "wnk012";
+    private static final String PASSWORD = "wnk012";
+    private static final String CONN_STRING = "jdbc:mysql://A2B:3306/wnk012";
+    
+    
+    
+}
