32a58b86812bb10ded21408d6161d1ccaa39364d
default-kaas <dogen1996@live.nl>
2017-05-24 12:46:57 +0200

    Hoop ?

--- FromAtoBBackoffice/src/fromatobbackoffice/MemberScreen.java
+++ FromAtoBBackoffice/src/fromatobbackoffice/MemberScreen.java
@@ -15,11 +15,38 @@ import net.proteanit.sql.DbUtils;
  */
 public class MemberScreen extends javax.swing.JFrame {
 
+    private javax.swing.JButton JButtonAllMembers;
+    private javax.swing.JButton jBEmail;
+    private javax.swing.JButton jBID;
+    private javax.swing.JButton jBStad;
+    private javax.swing.JButton jBachternaam;
+    private javax.swing.JButton jBorderDelivered;
+    private javax.swing.JButton jBorderPoints;
+    private javax.swing.JButton jBorderSent;
+    private javax.swing.JButton jBstartscreen;
+    private javax.swing.JButton jBvoornaam;
+    private javax.swing.JLabel jLEmail;
+    private javax.swing.JLabel jLID;
+    private javax.swing.JLabel jLStad;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLachternaam;
+    private javax.swing.JLabel jLvoornaam;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTFEmail;
+    private javax.swing.JTextField jTFID;
+    private javax.swing.JTextField jTFStad;
+    private javax.swing.JTextField jTFachternaam;
+    private javax.swing.JTextField jTFvoornaam;
+    private javax.swing.JTable jTable1;
+    
     Connection conn = null;
     Statement stmt = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
-    boolean hoogNaarLaag = true; //voor filteren
+    boolean hoogNaarLaag = true; 
 
     public MemberScreen() {
         initComponents();
@@ -698,67 +725,7 @@ public class MemberScreen extends javax.swing.JFrame {
         fetchLastname();
     }                                             
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MemberScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MemberScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MemberScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MemberScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MemberScreen().setVisible(true);
-            }
-        });
-    }
-
-    // Variables declaration - do not modify                     
-    private javax.swing.JButton JButtonAllMembers;
-    private javax.swing.JButton jBEmail;
-    private javax.swing.JButton jBID;
-    private javax.swing.JButton jBStad;
-    private javax.swing.JButton jBachternaam;
-    private javax.swing.JButton jBorderDelivered;
-    private javax.swing.JButton jBorderPoints;
-    private javax.swing.JButton jBorderSent;
-    private javax.swing.JButton jBstartscreen;
-    private javax.swing.JButton jBvoornaam;
-    private javax.swing.JLabel jLEmail;
-    private javax.swing.JLabel jLID;
-    private javax.swing.JLabel jLStad;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLachternaam;
-    private javax.swing.JLabel jLvoornaam;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTFEmail;
-    private javax.swing.JTextField jTFID;
-    private javax.swing.JTextField jTFStad;
-    private javax.swing.JTextField jTFachternaam;
-    private javax.swing.JTextField jTFvoornaam;
-    private javax.swing.JTable jTable1;
-    // End of variables declaration                   
+                       
+   
+                      
 }
